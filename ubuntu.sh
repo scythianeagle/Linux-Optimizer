999986877ffa7ec76f5afd7a089286528b39a5f3
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-05-04 14:43:25 +0330

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -266,41 +266,13 @@ sysctl_optimizations() {
     sleep 0.5
 
     sed -i -e '/fs.file-max/d' \
-        -e '/net.core.default_qdisc/d' \
-        -e '/net.core.netdev_max_backlog/d' \
-        -e '/net.core.optmem_max/d' \
-        -e '/net.core.somaxconn/d' \
-        -e '/net.core.rmem_max/d' \
-        -e '/net.core.wmem_max/d' \
-        -e '/net.core.rmem_default/d' \
-        -e '/net.core.wmem_default/d' \
-        -e '/net.ipv4.tcp_rmem/d' \
-        -e '/net.ipv4.tcp_wmem/d' \
-        -e '/net.ipv4.tcp_congestion_control/d' \
-        -e '/net.ipv4.tcp_fastopen/d' \
-        -e '/net.ipv4.tcp_fin_timeout/d' \
-        -e '/net.ipv4.tcp_keepalive_time/d' \
-        -e '/net.ipv4.tcp_keepalive_probes/d' \
-        -e '/net.ipv4.tcp_keepalive_intvl/d' \
-        -e '/net.ipv4.tcp_max_orphans/d' \
-        -e '/net.ipv4.tcp_max_syn_backlog/d' \
-        -e '/net.ipv4.tcp_max_tw_buckets/d' \
-        -e '/net.ipv4.tcp_mem/d' \
-        -e '/net.ipv4.tcp_mtu_probing/d' \
-        -e '/net.ipv4.tcp_notsent_lowat/d' \
-        -e '/net.ipv4.tcp_retries2/d' \
         -e '/net.ipv4.tcp_sack/d' \
         -e '/net.ipv4.tcp_dsack/d' \
         -e '/net.ipv4.tcp_slow_start_after_idle/d' \
         -e '/net.ipv4.tcp_window_scaling/d' \
-        -e '/net.ipv4.tcp_adv_win_scale/d' \
         -e '/net.ipv4.tcp_ecn/d' \
         -e '/net.ipv4.tcp_ecn_fallback/d' \
         -e '/net.ipv4.tcp_syncookies/d' \
-        -e '/net.ipv4.udp_mem/d' \
-        -e '/net.ipv6.conf.all.disable_ipv6/d' \
-        -e '/net.ipv6.conf.default.disable_ipv6/d' \
-        -e '/net.ipv6.conf.lo.disable_ipv6/d' \
         -e '/net.unix.max_dgram_qlen/d' \
         -e '/vm.min_free_kbytes/d' \
         -e '/vm.swappiness/d' \
@@ -309,11 +281,6 @@ sysctl_optimizations() {
         -e '/net.ipv4.conf.all.rp_filter/d' \
         -e '/net.ipv4.conf.all.accept_source_route/d' \
         -e '/net.ipv4.conf.default.accept_source_route/d' \
-        -e '/net.ipv4.neigh.default.gc_thresh1/d' \
-        -e '/net.ipv4.neigh.default.gc_thresh2/d' \
-        -e '/net.ipv4.neigh.default.gc_thresh3/d' \
-        -e '/net.ipv4.neigh.default.gc_stale_time/d' \
-        -e '/net.ipv4.conf.default.arp_announce/d' \
         -e '/net.ipv4.conf.lo.arp_announce/d' \
         -e '/net.ipv4.conf.all.arp_announce/d' \
         -e '/kernel.panic/d' \
@@ -348,72 +315,72 @@ fs.file-max = 67108864
 ## ----------------------------------------------------------------
 
 # Specify default queuing discipline for network devices
-#net.core.default_qdisc = fq_codel
+# net.core.default_qdisc = fq_codel
 
 # Configure maximum network device backlog
-net.core.netdev_max_backlog = 32768
+# net.core.netdev_max_backlog = 32768
 
 # Set maximum socket receive buffer
-net.core.optmem_max = 262144
+# net.core.optmem_max = 262144
 
 # Define maximum backlog of pending connections
-net.core.somaxconn = 65536
+# net.core.somaxconn = 65536
 
 # Configure maximum TCP receive buffer size
-net.core.rmem_max = 33554432
+# net.core.rmem_max = 33554432
 
 # Set default TCP receive buffer size
-net.core.rmem_default = 1048576
+# net.core.rmem_default = 1048576
 
 # Configure maximum TCP send buffer size
-net.core.wmem_max = 33554432
+# net.core.wmem_max = 33554432
 
 # Set default TCP send buffer size
-net.core.wmem_default = 1048576
+# net.core.wmem_default = 1048576
 
 
 ## TCP settings
 ## ----------------------------------------------------------------
 
 # Define socket receive buffer sizes
-net.ipv4.tcp_rmem = 16384 1048576 33554432
+# net.ipv4.tcp_rmem = 16384 1048576 33554432
 
 # Specify socket send buffer sizes
-net.ipv4.tcp_wmem = 16384 1048576 33554432
+# net.ipv4.tcp_wmem = 16384 1048576 33554432
 
 # Set TCP congestion control algorithm to HYBLA
 net.ipv4.tcp_congestion_control = cubic
 
 # Configure TCP FIN timeout period
-net.ipv4.tcp_fin_timeout = 25
+# net.ipv4.tcp_fin_timeout = 25
 
 # Set keepalive time (seconds)
-net.ipv4.tcp_keepalive_time = 1200
+# net.ipv4.tcp_keepalive_time = 1200
 
 # Configure keepalive probes count and interval
-net.ipv4.tcp_keepalive_probes = 7
-net.ipv4.tcp_keepalive_intvl = 30
+# net.ipv4.tcp_keepalive_probes = 7
+# net.ipv4.tcp_keepalive_intvl = 30
 
 # Define maximum orphaned TCP sockets
-net.ipv4.tcp_max_orphans = 819200
+# net.ipv4.tcp_max_orphans = 819200
 
 # Set maximum TCP SYN backlog
-net.ipv4.tcp_max_syn_backlog = 20480
+# net.ipv4.tcp_max_syn_backlog = 20480
 
 # Configure maximum TCP Time Wait buckets
-net.ipv4.tcp_max_tw_buckets = 1440000
+# net.ipv4.tcp_max_tw_buckets = 1440000
 
 # Define TCP memory limits
-net.ipv4.tcp_mem = 65536 1048576 33554432
+# net.ipv4.tcp_mem = 65536 1048576 33554432
 
 # Enable TCP MTU probing
 net.ipv4.tcp_mtu_probing = 1
 
 # Define minimum amount of data in the send buffer before TCP starts sending
-net.ipv4.tcp_notsent_lowat = 32768
+# net.ipv4.tcp_notsent_lowat = 32768
 
 # Specify retries for TCP socket to establish connection
-net.ipv4.tcp_retries2 = 8
+# net.ipv4.tcp_retries2 = 8
 
 # Enable TCP SACK and DSACK
 net.ipv4.tcp_sack = 1
@@ -424,7 +391,7 @@ net.ipv4.tcp_slow_start_after_idle = 0
 
 # Enable TCP window scaling
 net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_adv_win_scale = -2
+# net.ipv4.tcp_adv_win_scale = -2
 
 # Enable TCP ECN
 net.ipv4.tcp_ecn = 1
@@ -456,27 +423,27 @@ net.ipv6.conf.default.accept_ra = 0
 ## ----------------------------------------------------------------
 
 # Define UDP memory limits
-net.ipv4.udp_mem = 65536 1048576 33554432
+# net.ipv4.udp_mem = 65536 1048576 33554432
 
 
 ## IPv6 settings
 ## ----------------------------------------------------------------
 
 # Enable IPv6
-#net.ipv6.conf.all.disable_ipv6 = 0
+# net.ipv6.conf.all.disable_ipv6 = 0
 
 # Enable IPv6 by default
-#net.ipv6.conf.default.disable_ipv6 = 0
+# net.ipv6.conf.default.disable_ipv6 = 0
 
 # Enable IPv6 on the loopback interface (lo)
-#net.ipv6.conf.lo.disable_ipv6 = 0
+# net.ipv6.conf.lo.disable_ipv6 = 0
 
 
 ## UNIX domain sockets
 ## ----------------------------------------------------------------
 
 # Set maximum queue length of UNIX domain sockets
-net.unix.max_dgram_qlen = 256
+# net.unix.max_dgram_qlen = 256
 
 
 ## Virtual memory (VM) settings
@@ -504,10 +471,10 @@ net.ipv4.conf.all.accept_source_route = 0
 net.ipv4.conf.default.accept_source_route = 0
 
 # Neighbor table settings
-net.ipv4.neigh.default.gc_thresh1 = 512
-net.ipv4.neigh.default.gc_thresh2 = 2048
-net.ipv4.neigh.default.gc_thresh3 = 16384
-net.ipv4.neigh.default.gc_stale_time = 60
+# net.ipv4.neigh.default.gc_thresh1 = 512
+# net.ipv4.neigh.default.gc_thresh2 = 2048
+# net.ipv4.neigh.default.gc_thresh3 = 16384
+# net.ipv4.neigh.default.gc_stale_time = 60
 
 # ARP settings
 net.ipv4.conf.default.arp_announce = 2
