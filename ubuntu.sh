bf7590c0853460aec3b7054c15cef07e1412fc75
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-05-29 03:38:49 +0330

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -363,7 +363,7 @@ net.ipv6.conf.default.accept_source_route = 0
 net.ipv4.tcp_ecn = 1
 # net.ipv4.tcp_fastopen = 3
 # net.core.default_qdisc = fq_codel
-net.ipv4.tcp_congestion_control = cubic
+net.ipv4.tcp_congestion_control = hybla
 
 
 ## UDP settings
