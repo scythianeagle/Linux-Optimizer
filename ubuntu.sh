941074528285986dfb4e0d0f59f0697339b8e3b5
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-10-16 03:12:43 +0330

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -298,66 +298,92 @@ cat <<EOF >> "$SYS_PATH"
 # Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
 
 
-# max open files
-fs.file-max = 51200
-# max read buffer
-net.core.rmem_max = 67108864
-# max write buffer
-net.core.wmem_max = 67108864
-# default read buffer
-net.core.rmem_default = 65536
-# default write buffer
-net.core.wmem_default = 65536
-# max processor input queue
-net.core.netdev_max_backlog = 4096
-# max backlog
-net.core.somaxconn = 4096
-
-# resist SYN flood attacks
-net.ipv4.tcp_syncookies = 1
-# reuse timewait sockets when safe
-net.ipv4.tcp_tw_reuse = 1
-# turn off fast timewait sockets recycling
-# net.ipv4.tcp_tw_recycle = 0
-# short FIN timeout
-net.ipv4.tcp_fin_timeout = 30
-# short keepalive time
-net.ipv4.tcp_keepalive_time = 1200
-# outbound port range
-net.ipv4.ip_local_port_range = 10000 65000
-# max SYN backlog
-net.ipv4.tcp_max_syn_backlog = 4096
-# max timewait sockets held by system simultaneously
-net.ipv4.tcp_max_tw_buckets = 5000
-# turn on TCP Fast Open on both client and server side
-net.ipv4.tcp_fastopen = 3
-# TCP receive buffer
-net.ipv4.tcp_rmem = 4096 87380 67108864
-# TCP write buffer
-net.ipv4.tcp_wmem = 4096 65536 67108864
-# turn on path MTU discovery
-# net.ipv4.tcp_mtu_probing = 1
-
-# Emam config
 net.ipv4.ip_forward = 1
-net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0
-net.ipv4.conf.all.secure_redirects = 0
-net.ipv4.conf.default.secure_redirects = 0
-net.ipv6.conf.all.accept_redirects = 0
-net.ipv6.conf.default.accept_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
-net.ipv4.icmp_echo_ignore_all = 1
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv4.conf.default.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0
-net.ipv6.conf.default.accept_source_route = 0
-net.ipv4.tcp_ecn = 0
-net.ipv4.tcp_fastopen = 3
-# net.core.default_qdisc = fq_codel
-# net.ipv4.tcp_congestion_control = bbr
+fs.file-max = 67108864
+net.core.default_qdisc = fq_codel
+
+# Configure maximum network device backlog
+net.core.netdev_max_backlog = 32768
+
+# Set maximum socket receive buffer
+# net.core.optmem_max = 262144
+
+# Define maximum backlog of pending connections
+net.core.somaxconn = 65536
+
+# Configure maximum TCP receive buffer size
+# net.core.rmem_max = 33554432
+
+# Set default TCP receive buffer size
+# net.core.rmem_default = 1048576
+
+# Configure maximum TCP send buffer size
+# net.core.wmem_max = 33554432
+
+# Set default TCP send buffer size
+# net.core.wmem_default = 1048576
+
+## TCP settings
+## ----------------------------------------------------------------
+
+# Define socket receive buffer sizes
+# net.ipv4.tcp_rmem = 16384 1048576 33554432
+
+# Specify socket send buffer sizes
+net.ipv4.tcp_wmem = 16384 1048576 33554432
+
+# net.ipv4.tcp_wmem = 4096 131072 67108864
+
+net.ipv4.tcp_congestion_control = bbr
+
+# Configure TCP FIN timeout period
+net.ipv4.tcp_fin_timeout = 25
+
+# Set keepalive time (seconds)
+net.ipv4.tcp_keepalive_time = 1200
+
+# Configure keepalive probes count and interval
+net.ipv4.tcp_keepalive_probes = 7
+net.ipv4.tcp_keepalive_intvl = 30
+
+# Define maximum orphaned TCP sockets
+net.ipv4.tcp_max_orphans = 819200
+
+# Set maximum TCP SYN backlog
+net.ipv4.tcp_max_syn_backlog = 20480
+
+# Configure maximum TCP Time Wait buckets
+net.ipv4.tcp_max_tw_buckets = 1440000
+
+# Define TCP memory limits
+net.ipv4.tcp_mem = 65536 1048576 33554432
+
+# Enable TCP MTU probing
+net.ipv4.tcp_mtu_probing = 0
+
+# Define minimum amount of data in the send buffer before TCP starts sending
+net.ipv4.tcp_notsent_lowat = 32768
+
+# Specify retries for TCP socket to establish connection
+net.ipv4.tcp_retries2 = 8
+
+# Enable TCP SACK and DSACK
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_dsack = 1
+
+# Disable TCP slow start after idle
+net.ipv4.tcp_slow_start_after_idle = 0
+
+# Enable TCP window scaling
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = -2
+
+# Enable TCP ECN
+net.ipv4.tcp_ecn = 1
+net.ipv4.tcp_ecn_fallback = 1
+
+# Enable the use of TCP SYN cookies to help protect against SYN flood attacks
+net.ipv4.tcp_syncookies = 1
 
 
 ## UDP settings
@@ -365,7 +391,6 @@ net.ipv4.tcp_fastopen = 3
 
 # Define UDP memory limits
 net.ipv4.udp_mem = 65536 1048576 33554432
-
 
 ## IPv6 settings
 ## ----------------------------------------------------------------
@@ -384,7 +409,7 @@ net.ipv4.udp_mem = 65536 1048576 33554432
 ## ----------------------------------------------------------------
 
 # Set maximum queue length of UNIX domain sockets
-# net.unix.max_dgram_qlen = 256
+net.unix.max_dgram_qlen = 256
 
 
 ## Virtual memory (VM) settings
@@ -399,34 +424,50 @@ vm.swappiness = 10
 # Set the tendency of the kernel to reclaim memory used for caching of directory and inode objects
 vm.vfs_cache_pressure = 250
 
-
-## Network Configuration
-## ----------------------------------------------------------------
-
 # Configure reverse path filtering
-# net.ipv4.conf.default.rp_filter = 2
-# net.ipv4.conf.all.rp_filter = 2
-
-# Disable source route acceptance
-# net.ipv4.conf.all.accept_source_route = 0
-# net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.default.rp_filter = 2
+net.ipv4.conf.all.rp_filter = 2
 
 # Neighbor table settings
-# net.ipv4.neigh.default.gc_thresh1 = 512
-# net.ipv4.neigh.default.gc_thresh2 = 2048
-# net.ipv4.neigh.default.gc_thresh3 = 16384
-# net.ipv4.neigh.default.gc_stale_time = 60
+net.ipv4.neigh.default.gc_thresh1 = 512
+net.ipv4.neigh.default.gc_thresh2 = 2048
+net.ipv4.neigh.default.gc_thresh3 = 16384
+net.ipv4.neigh.default.gc_stale_time = 60
 
 # ARP settings
-# net.ipv4.conf.default.arp_announce = 2
-# net.ipv4.conf.lo.arp_announce = 2
-# net.ipv4.conf.all.arp_announce = 2
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
 
 # Kernel panic timeout
 kernel.panic = 1
 
 # Set dirty page ratio for virtual memory
 vm.dirty_ratio = 20
+
+
+# Emam config
+# source route acceptance
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.icmp_echo_ignore_all = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
+
+
+# net.ipv4.tcp_ecn = 0
+# net.ipv4.tcp_fastopen = 3
+# net.core.default_qdisc = fq_codel
+# net.ipv4.tcp_congestion_control = bbr
 
 
 ################################################################
