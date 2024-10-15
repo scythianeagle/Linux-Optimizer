fe4d8836ca7f4b6349b1fc17fe2a38bedefcbacd
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-10-16 03:20:11 +0330

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -944,39 +944,6 @@ apply_everything() {
 }
 
 
-# Generate self-signed SSL certificate
-SSL_certificate() {
-country=US
-state=New_York
-locality=New_York
-organization=atc
-organizationalunit=tower
-commonname=emam
-email=admin@hidessh.com
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/stunnel/stunnel.key -out /etc/stunnel/stunnel.crt -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-
-# Create a stunnel configuration file
-sudo touch /etc/stunnel/stunnel.conf
-sudo tee -a /etc/stunnel/stunnel.conf > /dev/null <<EOT
-cert = /etc/stunnel/stunnel.crt
-key = /etc/stunnel/stunnel.key
-
-[ssh]
-accept = 444
-connect = 127.0.0.1:1899
-EOT
-
-# Enable stunnel service
-sudo sed -i 's/ENABLED=0/ENABLED=1/' /etc/default/stunnel4
-
-# Restart stunnel service
-sudo systemctl restart stunnel4
-
-echo "SSL tunnel setup completed!"
-
-}
-
-
 main
 
 
