fcb201578f8a5f3561cf8cccd309b61a776e9a46
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2025-01-28 18:40:54 +0000

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -291,6 +291,7 @@ cat <<EOF >> "$SYS_PATH"
 # /etc/sysctl.conf
 # These parameters in this file will be added/updated to the sysctl.conf file.
 # Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
+
 net.ipv4.ip_forward=1
 fs.file-max=51200
 net.core.rmem_max=67108864
@@ -311,6 +312,24 @@ net.ipv4.tcp_fastopen=3
 net.ipv4.tcp_rmem=4096 87380 67108864
 net.ipv4.tcp_wmem=4096 65536 67108864
 net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default.accept_redirects=0
+net.ipv4.conf.all.secure_redirects=0
+net.ipv4.conf.default.secure_redirects=0
+net.ipv6.conf.all.accept_redirects=0
+net.ipv6.conf.default.accept_redirects=0
+net.ipv4.conf.default.send_redirects=0
+net.ipv4.icmp_echo_ignore_all=1
+net.ipv4.conf.all.accept_source_route=0
+net.ipv4.conf.default.accept_source_route=0
+net.ipv6.conf.all.accept_source_route=0
+net.ipv6.conf.default.accept_source_route=0
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
 
 
 
