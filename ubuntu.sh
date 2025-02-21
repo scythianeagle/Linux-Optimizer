817fae44f72c35c6380c8a07e9948128e03649be
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2025-02-21 14:56:50 +0330

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# https://github.com/hawshemi/Linux-Optimizer
+
 
 # Green, Yellow & Red Messages.
 green_msg() {
@@ -52,7 +54,7 @@ ask_reboot() {
     yellow_msg 'Reboot now? (Recommended) (y/n)'
     echo 
     while true; do
-        read -r choice
+        read choice
         echo 
         if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
             sleep 0.5
@@ -73,19 +75,19 @@ complete_update() {
     echo 
     sleep 0.5
 
-     apt -q update
-     apt -y upgrade
-     apt -y full-upgrade
-     apt -y autoremove
+    sudo apt -q update
+    sudo apt -y upgrade
+    sudo apt -y full-upgrade
+    sudo apt -y autoremove
     sleep 0.5
 
     ## Again :D
-     apt -y -q autoclean
-     apt -y clean
-     apt -q update
-     apt -y upgrade
-     apt -y full-upgrade
-     apt -y autoremove --purge
+    sudo apt -y -q autoclean
+    sudo apt -y clean
+    sudo apt -q update
+    sudo apt -y upgrade
+    sudo apt -y full-upgrade
+    sudo apt -y autoremove --purge
 
     echo 
     green_msg 'System Updated & Cleaned Successfully.'
@@ -129,9 +131,9 @@ install_xanmod() {
         sleep 0.5
 
         ## Update, Upgrade & Install dependencies
-         apt update -q
-         apt upgrade -y
-         apt install wget curl gpg -y
+        sudo apt update -q
+        sudo apt upgrade -y
+        sudo apt install wget curl gpg -y
 
         ## Check the CPU level
         cpu_level=$(awk -f - <<EOF
@@ -167,20 +169,20 @@ EOF
 
             # If we reach this point, it means we have a non-empty GPG file
             # Now dearmor the GPG key and move to the final location
-             gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg $tmp_keyring
+            sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg $tmp_keyring
 
             # Clean up the temporary file
             rm -f $tmp_keyring
 
             ## Add the XanMod repository
-            echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' |  tee /etc/apt/sources.list.d/xanmod-release.list
+            echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
             
             ## Install XanMod
-             apt update -q &&  apt install "linux-xanmod-x64v$cpu_level" -y
+            sudo apt update -q && sudo apt install "linux-xanmod-x64v$cpu_level" -y
 
             ## Clean up
-             apt update -q
-             apt autoremove --purge -y
+            sudo apt update -q
+            sudo apt autoremove --purge -y
             
             echo 
             green_msg "XanMod Kernel Installed. Reboot to Apply the new Kernel."
@@ -205,8 +207,20 @@ installations() {
     sleep 0.5
 
     ## Networking packages
-     apt -y install apt-transport-https stunnel4 openssh-server bash-completion ca-certificates apt-utils curl nano screen net-tools unzip wget tmux nethogs git pkg-config python3 python3-pip
-   
+    sudo apt -y install apt-transport-https
+
+    ## System utilities
+    sudo apt -y install apt-utils bash-completion busybox ca-certificates cron curl gnupg2 locales lsb-release nano preload screen software-properties-common unzip vim wget xxd zip
+
+    ## Programming and development tools
+    sudo apt -y install autoconf automake bash-completion build-essential git libtool make pkg-config python3 python3-pip
+
+    ## Additional libraries and dependencies
+    sudo apt -y install bc binutils binutils-common binutils-x86-64-linux-gnu ubuntu-keyring haveged jq libsodium-dev libsqlite3-dev libssl-dev packagekit qrencode socat
+
+    ## Miscellaneous
+    sudo apt -y install dialog htop net-tools
+
     echo 
     green_msg 'Useful Packages Installed Succesfully.'
     echo 
@@ -215,13 +229,13 @@ installations() {
 
 
 # Enable packages at server boot
-#enable_packages() {
-#     systemctl enable cron haveged preload
-#    echo 
-#    green_msg 'Packages Enabled Succesfully.'
-#    echo
-#    sleep 0.5
-#}
+enable_packages() {
+    sudo systemctl enable cron haveged preload
+    echo 
+    green_msg 'Packages Enabled Successfully.'
+    echo
+    sleep 0.5
+}
 
 
 ## Swap Maker
@@ -232,10 +246,10 @@ swap_maker() {
     sleep 0.5
 
     ## Make Swap
-     fallocate -l $SWAP_SIZE $SWAP_PATH  ## Allocate size
-     chmod 600 $SWAP_PATH                ## Set proper permission
-     mkswap $SWAP_PATH                   ## Setup swap         
-     swapon $SWAP_PATH                   ## Enable swap
+    sudo fallocate -l $SWAP_SIZE $SWAP_PATH  ## Allocate size
+    sudo chmod 600 $SWAP_PATH                ## Set proper permission
+    sudo mkswap $SWAP_PATH                   ## Setup swap         
+    sudo swapon $SWAP_PATH                   ## Enable swap
     echo "$SWAP_PATH   none    swap    sw    0   0" >> /etc/fstab ## Add to fstab
     echo 
     green_msg 'SWAP Created Successfully.'
@@ -260,26 +274,64 @@ sysctl_optimizations() {
     sleep 0.5
 
     sed -i -e '/fs.file-max/d' \
+        -e '/net.core.default_qdisc/d' \
+        -e '/net.core.netdev_max_backlog/d' \
+        -e '/net.core.optmem_max/d' \
+        -e '/net.core.somaxconn/d' \
+        -e '/net.core.rmem_max/d' \
+        -e '/net.core.wmem_max/d' \
+        -e '/net.core.rmem_default/d' \
+        -e '/net.core.wmem_default/d' \
+        -e '/net.ipv4.tcp_rmem/d' \
+        -e '/net.ipv4.tcp_wmem/d' \
+        -e '/net.ipv4.tcp_congestion_control/d' \
+        -e '/net.ipv4.tcp_fastopen/d' \
+        -e '/net.ipv4.tcp_fin_timeout/d' \
+        -e '/net.ipv4.tcp_keepalive_time/d' \
+        -e '/net.ipv4.tcp_keepalive_probes/d' \
+        -e '/net.ipv4.tcp_keepalive_intvl/d' \
+        -e '/net.ipv4.tcp_max_orphans/d' \
+        -e '/net.ipv4.tcp_max_syn_backlog/d' \
+        -e '/net.ipv4.tcp_max_tw_buckets/d' \
+        -e '/net.ipv4.tcp_mem/d' \
+        -e '/net.ipv4.tcp_mtu_probing/d' \
+        -e '/net.ipv4.tcp_notsent_lowat/d' \
+        -e '/net.ipv4.tcp_retries2/d' \
+        -e '/net.ipv4.tcp_sack/d' \
         -e '/net.ipv4.tcp_dsack/d' \
         -e '/net.ipv4.tcp_slow_start_after_idle/d' \
         -e '/net.ipv4.tcp_window_scaling/d' \
+        -e '/net.ipv4.tcp_adv_win_scale/d' \
         -e '/net.ipv4.tcp_ecn/d' \
         -e '/net.ipv4.tcp_ecn_fallback/d' \
         -e '/net.ipv4.tcp_syncookies/d' \
+        -e '/net.ipv4.udp_mem/d' \
+        -e '/net.ipv6.conf.all.disable_ipv6/d' \
+        -e '/net.ipv6.conf.default.disable_ipv6/d' \
+        -e '/net.ipv6.conf.lo.disable_ipv6/d' \
         -e '/net.unix.max_dgram_qlen/d' \
+        -e '/vm.min_free_kbytes/d' \
         -e '/vm.swappiness/d' \
+        -e '/vm.vfs_cache_pressure/d' \
         -e '/net.ipv4.conf.default.rp_filter/d' \
         -e '/net.ipv4.conf.all.rp_filter/d' \
         -e '/net.ipv4.conf.all.accept_source_route/d' \
         -e '/net.ipv4.conf.default.accept_source_route/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh1/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh2/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh3/d' \
+        -e '/net.ipv4.neigh.default.gc_stale_time/d' \
+        -e '/net.ipv4.conf.default.arp_announce/d' \
         -e '/net.ipv4.conf.lo.arp_announce/d' \
         -e '/net.ipv4.conf.all.arp_announce/d' \
+        -e '/kernel.panic/d' \
+        -e '/vm.dirty_ratio/d' \
         -e '/^#/d' \
         -e '/^$/d' \
         "$SYS_PATH"
 
 
-    ## Add new parameteres. Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
+    ## Add new parameters. Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
 
 cat <<EOF >> "$SYS_PATH"
 
@@ -292,47 +344,192 @@ cat <<EOF >> "$SYS_PATH"
 # These parameters in this file will be added/updated to the sysctl.conf file.
 # Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
 
-net.ipv4.ip_forward=1
-fs.file-max=51200
-net.core.rmem_max=67108864
-net.core.wmem_max=67108864
-net.core.rmem_default=65536
-net.core.wmem_default=65536
-net.core.netdev_max_backlog=4096
-net.core.somaxconn=4096
-net.ipv4.tcp_syncookies=1
-net.ipv4.tcp_tw_reuse=1
-net.ipv4.tcp_tw_recycle=0
-net.ipv4.tcp_fin_timeout=30
-net.ipv4.tcp_keepalive_time=1200
-net.ipv4.ip_local_port_range=10000 65000
-net.ipv4.tcp_max_syn_backlog=4096
-net.ipv4.tcp_max_tw_buckets=5000
-net.ipv4.tcp_fastopen=3
-net.ipv4.tcp_rmem=4096 87380 67108864
-net.ipv4.tcp_wmem=4096 65536 67108864
+
+## File system settings
+## ----------------------------------------------------------------
+
+# Set the maximum number of open file descriptors
+fs.file-max = 67108864
+
+
+## Network core settings
+## ----------------------------------------------------------------
+
+# Specify default queuing discipline for network devices
+net.core.default_qdisc = fq
+
+# Configure maximum network device backlog
+net.core.netdev_max_backlog = 32768
+
+# Set maximum socket receive buffer
+net.core.optmem_max = 262144
+
+# Define maximum backlog of pending connections
+net.core.somaxconn = 65536
+
+# Configure maximum TCP receive buffer size
+net.core.rmem_max = 33554432
+
+# Set default TCP receive buffer size
+net.core.rmem_default = 1048576
+
+# Configure maximum TCP send buffer size
+net.core.wmem_max = 33554432
+
+# Set default TCP send buffer size
+net.core.wmem_default = 1048576
+
+
+## TCP settings
+## ----------------------------------------------------------------
+
+# Define socket receive buffer sizes
+net.ipv4.tcp_rmem = 16384 1048576 33554432
+
+# Specify socket send buffer sizes
+net.ipv4.tcp_wmem = 16384 1048576 33554432
+
+# Set TCP congestion control algorithm to BBR
+net.ipv4.tcp_congestion_control = bbr
+
+# Configure TCP FIN timeout period
+net.ipv4.tcp_fin_timeout = 25
+
+# Set keepalive time (seconds)
+net.ipv4.tcp_keepalive_time = 1200
+
+# Configure keepalive probes count and interval
+net.ipv4.tcp_keepalive_probes = 7
+net.ipv4.tcp_keepalive_intvl = 30
+
+# Define maximum orphaned TCP sockets
+net.ipv4.tcp_max_orphans = 819200
+
+# Set maximum TCP SYN backlog
+net.ipv4.tcp_max_syn_backlog = 20480
+
+# Configure maximum TCP Time Wait buckets
+net.ipv4.tcp_max_tw_buckets = 1440000
+
+# Define TCP memory limits
+net.ipv4.tcp_mem = 65536 1048576 33554432
+
+# Enable TCP MTU probing
+net.ipv4.tcp_mtu_probing = 0
+
+# Define minimum amount of data in the send buffer before TCP starts sending
+net.ipv4.tcp_notsent_lowat = 32768
+
+# Specify retries for TCP socket to establish connection
+net.ipv4.tcp_retries2 = 8
+
+# Enable TCP SACK and DSACK
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_dsack = 1
+
+# Disable TCP slow start after idle
+net.ipv4.tcp_slow_start_after_idle = 0
+
+# Enable TCP window scaling
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = -2
+
+# Enable TCP ECN
+net.ipv4.tcp_ecn = 1
+net.ipv4.tcp_ecn_fallback = 1
+
+# Enable the use of TCP SYN cookies to help protect against SYN flood attacks
+net.ipv4.tcp_syncookies = 1
+
+
+## UDP settings
+## ----------------------------------------------------------------
+
+# Define UDP memory limits
+net.ipv4.udp_mem = 65536 1048576 33554432
+
+
+## IPv6 settings
+## ----------------------------------------------------------------
+
+# Enable IPv6
+#net.ipv6.conf.all.disable_ipv6 = 0
+
+# Enable IPv6 by default
+#net.ipv6.conf.default.disable_ipv6 = 0
+
+# Enable IPv6 on the loopback interface (lo)
+#net.ipv6.conf.lo.disable_ipv6 = 0
+
+
+## UNIX domain sockets
+## ----------------------------------------------------------------
+
+# Set maximum queue length of UNIX domain sockets
+net.unix.max_dgram_qlen = 256
+
+
+## Virtual memory (VM) settings
+## ----------------------------------------------------------------
+
+# Specify minimum free Kbytes at which VM pressure happens
+vm.min_free_kbytes = 65536
+
+# Define how aggressively swap memory pages are used
+vm.swappiness = 10
+
+# Set the tendency of the kernel to reclaim memory used for caching of directory and inode objects
+#vm.vfs_cache_pressure = 250
+
+
+## Network Configuration
+## ----------------------------------------------------------------
+
+# Configure reverse path filtering
+net.ipv4.conf.default.rp_filter = 2
+net.ipv4.conf.all.rp_filter = 2
+
+# Disable source route acceptance
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+
+# Neighbor table settings
+net.ipv4.neigh.default.gc_thresh1 = 512
+net.ipv4.neigh.default.gc_thresh2 = 2048
+net.ipv4.neigh.default.gc_thresh3 = 16384
+net.ipv4.neigh.default.gc_stale_time = 60
+
+# ARP settings
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+
+# Kernel panic timeout
+kernel.panic = 1
+
+# Set dirty page ratio for virtual memory
+vm.dirty_ratio = 20
+
+# Emam config
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.icmp_echo_ignore_all = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_ra = 0
+net.ipv6.conf.default.accept_ra = 0
 net.ipv4.tcp_mtu_probing=0
 net.ipv4.tcp_slow_start_after_idle=0
-net.ipv4.conf.all.rp_filter=1
-net.ipv4.conf.default.rp_filter=1
-net.ipv4.conf.all.accept_redirects=0
-net.ipv4.conf.default.accept_redirects=0
-net.ipv4.conf.all.secure_redirects=0
-net.ipv4.conf.default.secure_redirects=0
-net.ipv6.conf.all.accept_redirects=0
-net.ipv6.conf.default.accept_redirects=0
-net.ipv4.conf.default.send_redirects=0
-net.ipv4.icmp_echo_ignore_all=1
-net.ipv4.conf.all.accept_source_route=0
-net.ipv4.conf.default.accept_source_route=0
-net.ipv6.conf.all.accept_source_route=0
-net.ipv6.conf.default.accept_source_route=0
-net.ipv6.conf.all.disable_ipv6=1
-net.ipv6.conf.default.disable_ipv6=1
-net.ipv6.conf.lo.disable_ipv6=1
-
-
-
 
 ################################################################
 ################################################################
@@ -340,8 +537,7 @@ net.ipv6.conf.lo.disable_ipv6=1
 
 EOF
 
-    sysctl -p
-    sysctl --system
+    sudo sysctl -p
     
     echo 
     green_msg 'Network is Optimized.'
@@ -412,18 +608,12 @@ update_sshd_conf() {
     echo 
     sleep 0.5
 
-    ## Change port
-    echo "Port 1899" | tee -a "$SSH_PATH"
-
     ## Enable TCP keep-alive messages
     echo "TCPKeepAlive yes" | tee -a "$SSH_PATH"
 
     ## Configure client keep-alive messages
     echo "ClientAliveInterval 3000" | tee -a "$SSH_PATH"
     echo "ClientAliveCountMax 100" | tee -a "$SSH_PATH"
-
-    ## Allow agent forwarding
-    echo "AllowAgentForwarding yes" | tee -a "$SSH_PATH"
 
     ## Allow TCP forwarding
     echo "AllowTcpForwarding yes" | tee -a "$SSH_PATH"
@@ -438,7 +628,7 @@ update_sshd_conf() {
     echo "X11Forwarding yes" | tee -a "$SSH_PATH"
 
     ## Restart the SSH service to apply the changes
-    systemctl restart ssh
+    sudo systemctl restart ssh
 
     echo 
     green_msg 'SSH is Optimized.'
@@ -519,6 +709,45 @@ limits_optimizations() {
     sleep 0.5
 }
 
+
+# UFW Optimizations
+#ufw_optimizations() {
+    #echo
+    #yellow_msg 'Installing & Optimizing UFW...'
+    #echo 
+    #sleep 0.5
+
+    ## Purge firewalld to install UFW.
+    #sudo apt -y purge firewalld
+    
+    ## Install UFW if it isn't installed.
+    #sudo apt update -q
+    #sudo apt install -y ufw
+
+    ## Disable UFW
+     #sudo ufw disable
+
+    ## Open default ports.
+    #sudo ufw allow $SSH_PORT
+    #sudo ufw allow 80/tcp
+    #sudo ufw allow 80/udp
+    #sudo ufw allow 443/tcp
+    #sudo ufw allow 443/udp
+    #sleep 0.5
+
+    ## Change the UFW config to use System config.
+    #sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
+
+    ## Enable & Reload
+    #echo "y" | sudo ufw enable
+    #sudo ufw reload
+    #echo 
+    #green_msg 'UFW is Installed & Optimized. (Open your custom ports manually.)'
+    #echo 
+    #sleep 0.5
+#}
+
+
 # Show the Menu
 show_menu() {
     echo 
@@ -528,23 +757,23 @@ show_menu() {
     echo
     green_msg '2  - Install XanMod Kernel.'
     echo 
-    green_msg '3  - Complete Update + Useful Packages + Make SWAP + Optimize Network, SSH & System Limits '
-    green_msg '4  - Complete Update + Make SWAP + Optimize Network, SSH & System Limits '
+    green_msg '3  - Complete Update + Useful Packages + Make SWAP + Optimize Network, SSH & System Limits'
+    green_msg '4  - Complete Update + Make SWAP + Optimize Network, SSH & System Limits'
     green_msg '5  - Complete Update + Make SWAP + Optimize Network, SSH & System Limits'
     echo 
     green_msg '6  - Complete Update & Clean the OS.'
     green_msg '7  - Install Useful Packages.'
-    green_msg '8  - Make SWAP (2Gb).'
+    green_msg '8  - Make SWAP (1Gb).'
     green_msg '9  - Optimize the Network, SSH & System Limits.'
     echo 
     green_msg '10 - Optimize the Network settings.'
     green_msg '11 - Optimize the SSH settings.'
     green_msg '12 - Optimize the System Limits.'
     echo 
-#    green_msg '13 - Install & Optimize .'
-#    echo 
-#    red_msg 'q - Exit.'
-#    echo 
+    green_msg '13 - Install & Optimize UFW.'
+    echo 
+    red_msg 'q - Exit.'
+    echo 
 }
 
 
@@ -552,7 +781,7 @@ show_menu() {
 main() {
     while true; do
         show_menu
-        read  -r -p 'Enter Your Choice: ' choice
+        read -p 'Enter Your Choice: ' choice
         case $choice in
         1)
             apply_everything
@@ -583,7 +812,8 @@ main() {
             complete_update
             sleep 0.5
 
-            installations_enable_packages
+            installations
+            enable_packages
             sleep 0.5
 
             swap_maker
@@ -601,9 +831,9 @@ main() {
             limits_optimizations
             sleep 0.5
 
-#            find_ssh_port
-#            _optimizations
-#           sleep 0.5
+            find_ssh_port
+            ufw_optimizations
+            sleep 0.5
 
             echo 
             green_msg '========================='
@@ -631,9 +861,9 @@ main() {
             limits_optimizations
             sleep 0.5
 
-#            find_ssh_port
-#            _optimizations
-#            sleep 0.5
+            find_ssh_port
+            #ufw_optimizations
+            sleep 0.5
 
             echo 
             green_msg '========================='
@@ -760,7 +990,17 @@ main() {
 
             ask_reboot
             ;;
+        13)
+            find_ssh_port
+            #ufw_optimizations
+            sleep 0.5
 
+            echo 
+            green_msg '========================='
+            green_msg  'Done.'
+            green_msg '========================='
+
+            ;;
         q)
             exit 0
             ;;
@@ -805,6 +1045,7 @@ apply_everything() {
     sleep 0.5
     
     find_ssh_port
+    #_optimizations
     sleep 0.5
 }
 
